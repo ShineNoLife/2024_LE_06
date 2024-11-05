4b7aa921d1394e9de7a93165d4ed1966f67499c6
ShineNoLife <haxuanthien1@gmail.com>
2024-11-05 21:48:02 +0700

    updated python_syntax

--- python_syntax.py
+++ python_syntax.py
@@ -1,5 +1,25 @@
+#-----INPUT AND OUTPUT IN PYTHON-----
+
 #input of multiple numbers, map them to a type
 #x, y, z = map(int, input("enter x, y, z: \n").split())
 
 #when printing multiple values, need to group them up like (x, y, z), can mix different types of values 
 #print("output is: %f %d %f" %(x, y, z))
+
+#-----FUNCTIONS IN PYTHON-----
+
+# def square(x):
+#     return x ** 2
+
+#this is also a way to define the type of a function
+#def square(x) -> int:
+#   return x**2 
+
+# def printList(*ls):
+#   for x in ls:
+#       print(x)
+#
+#   def printOdd():
+#       for x in ls:
+#           if (x % 2)
+#               print(x)
