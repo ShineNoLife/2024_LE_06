a3355e11f4806433cc90a0fce3f1cd23548671d6
ShineNoLife <haxuanthien1@gmail.com>
2024-11-05 21:18:35 +0700

    first file

---
+++ python_syntax.py
@@ -0,0 +1,5 @@
+#input of multiple numbers, map them to a type
+#x, y, z = map(int, input("enter x, y, z: \n").split())
+
+#when printing multiple values, need to group them up like (x, y, z), can mix different types of values 
+#print("output is: %f %d %f" %(x, y, z))
