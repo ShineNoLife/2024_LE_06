95c23c9c585819d6a24ebcac2768f023747ab33c
ShineNoLife <haxuanthien1@gmail.com>
2024-11-07 11:35:35 +0700

    small updates

--- python_syntax.py
+++ python_syntax.py
@@ -1,10 +1,12 @@
 #-----INPUT AND OUTPUT IN PYTHON-----
 
 #input of multiple numbers, map them to a type
-#x, y, z = map(int, input("enter x, y, z: \n").split())
+# x, y, z = map(int, input("enter x, y, z: \n").split())
 
 #when printing multiple values, need to group them up like (x, y, z), can mix different types of values 
 #print("output is: %f %d %f" %(x, y, z))
+#or you can just use fstring
+# print(f"output is: {x: f} {y: d} {z: f}")
 
 #-----FUNCTIONS IN PYTHON-----
 
