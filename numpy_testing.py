d1b5855b1892bc2165f8e0275750db082c6f6978
ShineNoLife <haxuanthien1@gmail.com>
2024-11-07 11:47:53 +0700

    added numpy_testing

---
+++ numpy_testing.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+
+# Perform element-wise addition
+result_addition = array1 + array2
+
+# Display the original arrays and the results
+print("Array 1:", array1)
+print("Array 2:", array2)
+print("Element-wise Addition:", result_addition)
